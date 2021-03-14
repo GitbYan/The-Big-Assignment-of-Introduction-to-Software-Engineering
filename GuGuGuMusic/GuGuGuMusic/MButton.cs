ead4d919bc8fecae0013c8daadc9eccc2ab3538a
GitbYan <whustulby@gmail.com>
2021-03-14 21:12:43 +0800

    实现了界面的新建列表，实现了列表的切换显示，实现了用户点击列表中音乐立刻播放。

---
+++ GuGuGuMusic/GuGuGuMusic/MButton.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuGuGuMusic
+{
+    public class MButton:Button
+    {
+        public MButton()
+        {
+
+        }
+
+        public MButton(Music music)
+        {
+            _music = music;
+        }
+
+        private Music _music = new Music();
+        public Music M_music
+        {
+            get { return _music; }
+            set
+            {
+                _music = value;
+            }
+        }
+
+        public new event EventHandler DoubleClick;
+        DateTime clickTime;
+        bool isClicked = false;
+        protected override void OnClick(EventArgs e)
+        {
+            base.OnClick(e);
+            if (isClicked)
+            {
+                TimeSpan span = DateTime.Now - clickTime;
+                if (span.Milliseconds < SystemInformation.DoubleClickTime)
+                {
+                    DoubleClick(this, e);
+                    isClicked = false;
+                }
+                else
+                {
+                    isClicked = true;
+                    clickTime = DateTime.Now;
+                }
+            }
+            else
+            {
+                isClicked = true;
+                clickTime = DateTime.Now;
+            }
+        }
+
+    }
+
+
+}
---
+++ GuGuGuMusic/GuGuGuMusic/MLButton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuGuGuMusic
+{
+    public class MLButton:Button
+    {
+        public MLButton()
+        {
+
+        }
+        public MLButton(MGroup mGroup)
+        {
+            _mGroup = mGroup;
+        }
+
+        private MGroup _mGroup = new MGroup();
+        public MGroup M_mGroup
+        {
+            get { return _mGroup; }
+            set
+            {
+                _mGroup = value;
+            }
+        }
+    }
+}
